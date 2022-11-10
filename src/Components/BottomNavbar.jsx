ec8b6265b7d9d060659d6365c72931d76e1091cf
Salman <salmanajani0426@gmail.com>
2022-11-10 18:22:46 +0530

    added search bar and basic structure

--- src/Components/BottomNavbar.jsx
+++ src/Components/BottomNavbar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Container, HStack, Box } from "@chakra-ui/react";
+import { Container, HStack, Box, Divider } from "@chakra-ui/react";
 
 const BottomNavbar = () => {
   return (
-    <Container maxW="70%" centerContent marginTop="15px">
-      <HStack spacing="30px">
-        <Box>Categories</Box>
-        <Box>Flash Sales</Box>
-        <Box>Best Deals</Box>
-        <Box>Shop By Brand</Box>
-        <Box>Trending</Box>
-        <Box>New Arrivals</Box>
-      </HStack>
-    </Container>
+    <>
+      <Container maxW="70%" centerContent marginTop="15px" marginBottom="1rem">
+        <HStack spacing="30px">
+          <Box>Categories</Box>
+          <Box>Flash Sales</Box>
+          <Box>Best Deals</Box>
+          <Box>Shop By Brand</Box>
+          <Box>Trending</Box>
+          <Box>New Arrivals</Box>
+        </HStack>
+      </Container>
+      <Divider borderColor="black" />
+    </>
   );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Box, Heading, Grid, GridItem } from "@chakra-ui/react";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((res) => setData(res.products));
+  }, []);
+
   return (
     <>
-      <h1>HomePage</h1>
+      <Container
+        maxW="6xl"
+        centerContent
+        border="1px solid black"
+        marginTop="40px"
+        marginBottom="50px"
+      >
+        <Box padding="14" color="black" maxW="md">
+          There are many benefits to a joint design and development system. Not
+          only does it bring benefits to the design team, but it also brings
+          benefits to engineering teams. It makes sure that our experiences have
+          a consistent look and feel, not just in our design specs, but in
+          production.
+        </Box>
+      </Container>
+      <Container maxW="6xl" centerContent>
+        <Box marginBottom="20px">
+          <Heading as="h4" size="lg">
+            Fast Moving Products
+          </Heading>
+        </Box>
+        <Box marginBottom="60px">
+          <Grid templateColumns="repeat(5, 1fr)" gap={6} maxW="60%">
+            {data
+              .filter((item) => {
+                return item.category === "smartphones";
+              })
+              .map((product) => (
+                <GridItem w="300px" h="250px" bg="blue.500">
+                  {product.title}
+                </GridItem>
+              ))}
+          </Grid>
+        </Box>
+      </Container>
+      <Container maxW="6xl" centerContent>
+        <Box marginBottom="20px">
+          <Heading as="h4" size="lg">
+            Home Decor
+          </Heading>
+        </Box>
+        <Box marginBottom="60px">
+          <Grid templateColumns="repeat(5, 1fr)" gap={6} maxW="60%">
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+          </Grid>
+        </Box>
+      </Container>
+      <Container maxW="6xl" centerContent>
+        <Box marginBottom="20px">
+          <Heading as="h4" size="lg">
+            Small Appliances
+          </Heading>
+        </Box>
+        <Box marginBottom="60px">
+          <Grid templateColumns="repeat(5, 1fr)" gap={6} maxW="60%">
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+          </Grid>
+        </Box>
+      </Container>
+      <Container maxW="6xl" centerContent>
+        <Box marginBottom="20px">
+          <Heading as="h4" size="lg">
+            Watches
+          </Heading>
+        </Box>
+        <Box marginBottom="60px">
+          <Grid templateColumns="repeat(5, 1fr)" gap={6} maxW="60%">
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+          </Grid>
+        </Box>
+      </Container>
+      <Container maxW="6xl" centerContent>
+        <Box marginBottom="20px">
+          <Heading as="h4" size="lg">
+            Clothing
+          </Heading>
+        </Box>
+        <Box marginBottom="60px">
+          <Grid templateColumns="repeat(5, 1fr)" gap={6} maxW="60%">
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+            <GridItem w="300px" h="250px" bg="blue.500" />
+          </Grid>
+        </Box>
+      </Container>
     </>
   );
 };
